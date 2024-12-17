33582580f8e30b67a62c0b6a1da5cc77a05d723a
Pabo Azevedo <pablohga@gmail.com>
2024-12-17 16:44:34 +0000

    `Update app.json with new splash screen and adaptive icon background colors`

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+export default function Index(){
+  return (
+    <View style={{flex:1, 
+    justifyContent: "center",
+    alignItems: "center"}}>
+      <Text style={{fontSize: 22}}>Hell0000o</Text>
+    </View>
+  )
+}
